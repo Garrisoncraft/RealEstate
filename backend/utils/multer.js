22406f1a7732ea2787327cb77c859ef71917a8e6
Garrisoncraft <garrisonsayor@gmail.com>
2025-05-12 23:12:55 +0200

    Multer storage

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'property-images',
-    allowed_formats: ['jpg', 'png', 'jpeg'],
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
   },
 });
 
