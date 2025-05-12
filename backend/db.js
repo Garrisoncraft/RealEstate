2511371afd62701f1927ee72d515fd477ea120fc
Garrisoncraft <garrisonsayor@gmail.com>
2025-05-12 19:15:55 +0200

    Deployement fixes

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 let pool;
 
-if (process.env.MYSQL_URL) {
-  // Use connection string from environment variable if present
-  pool = mysql.createPool(process.env.MYSQL_URL);
+if (process.env.MYSQL_PUBLIC_URL) {
+
+  pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 } else {
   // Fallback to individual environment variables
   pool = mysql.createPool({
